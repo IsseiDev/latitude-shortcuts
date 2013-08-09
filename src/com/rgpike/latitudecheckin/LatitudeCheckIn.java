838cc461738a31752619a0e97c7601a3abdf863b
Ricky Pike <arcanericky@gmail.com>
2013-08-09 00:00:00 -0500

    Add buttons for all shortcuts

--- src/com/rgpike/latitudecheckin/LatitudeCheckIn.java
+++ src/com/rgpike/latitudecheckin/LatitudeCheckIn.java
@@ -9,6 +9,7 @@ import android.content.ComponentName;
 import android.net.Uri;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.view.View;
 
 public class LatitudeCheckIn extends Activity
 {
@@ -17,16 +18,19 @@ public class LatitudeCheckIn extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 
+	private void CreateShortcut(String title, String uri)
+	{
         Intent shortcut = new
             Intent("com.android.launcher.action.INSTALL_SHORTCUT");
 
-        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME,
-            "Check In");
-        shortcut.putExtra("duplicate", false);  // Just create once
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, title);
+        shortcut.putExtra("duplicate", false);
 
         Intent intent = new Intent("android.intent.action.VIEW",
-            Uri.parse("latitude://latitude/checkin"));
+			Uri.parse(uri));
         shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT,
             intent);
 
@@ -36,17 +40,118 @@ public class LatitudeCheckIn extends Activity
         shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes);
 
         sendBroadcast(shortcut);
+	}
 
-        setContentView(R.layout.main);
-
+	private void AlertCompleted(String title, String message)
+	{
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle("Shortcut Created");
-        alertDialog.setMessage("Your shortcut is named \"Check In\"");
-        alertDialog.setButton("OK", new DialogInterface.OnClickListener()
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(getResources().getString(R.string.OK),
+            new DialogInterface.OnClickListener()
             {
                 public void onClick(DialogInterface dialog, int which)
                     {
-                    finish();
+                    };
+            } );
+
+        alertDialog.show();
+	}
+
+    public void onCreateCheckInClick(View v)
+    {
+		CreateShortcut(
+            getResources().getString(R.string.TitleCheckin),
+            getResources().getString(R.string.LatitudeCheckinURI)
+			);
+
+		AlertCompleted(
+            getResources().getString(R.string.TitleCheckin),
+			String.format(
+            	getResources().getString(R.string.InstalledAlertMessage),
+            	getResources().getString(R.string.TitleCheckin))
+			);
+    }    
+
+    public void onCreateListClick(View v)
+    {
+		CreateShortcut(
+            getResources().getString(R.string.TitleList),
+            getResources().getString(R.string.LatitudeListURI)
+			);
+
+		AlertCompleted(
+            getResources().getString(R.string.TitleList),
+			String.format(
+            	getResources().getString(R.string.InstalledAlertMessage),
+            	getResources().getString(R.string.TitleList))
+			);
+	}
+
+    public void onCreateHistoryClick(View v)
+    {
+		CreateShortcut(
+            getResources().getString(R.string.TitleHistory),
+            getResources().getString(R.string.LatitudeHistoryURI)
+			);
+
+		AlertCompleted(
+            getResources().getString(R.string.TitleHistory),
+			String.format(
+            	getResources().getString(R.string.InstalledAlertMessage),
+            	getResources().getString(R.string.TitleHistory))
+			);
+	}
+
+    public void onCreatePlacesClick(View v)
+    {
+		CreateShortcut(
+            getResources().getString(R.string.TitlePlaces),
+            getResources().getString(R.string.LatitudePlacesURI)
+			);
+
+		AlertCompleted(
+            getResources().getString(R.string.TitlePlaces),
+			String.format(
+            	getResources().getString(R.string.InstalledAlertMessage),
+            	getResources().getString(R.string.TitlePlaces))
+			);
+	}
+
+    public void onCreateAllClick(View v)
+    {
+		CreateShortcut(
+            getResources().getString(R.string.TitleCheckin),
+            getResources().getString(R.string.LatitudeCheckinURI)
+			);
+		CreateShortcut(
+            getResources().getString(R.string.TitleList),
+            getResources().getString(R.string.LatitudeListURI)
+			);
+		CreateShortcut(
+            getResources().getString(R.string.TitleHistory),
+            getResources().getString(R.string.LatitudeHistoryURI)
+			);
+		CreateShortcut(
+            getResources().getString(R.string.TitlePlaces),
+            getResources().getString(R.string.LatitudePlacesURI)
+			);
+
+		AlertCompleted("All", "All");
+	}
+
+    public void onAboutClick(View v)
+    {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(
+            getResources().getString(R.string.AboutAlertTitle));
+        alertDialog.setMessage(
+            getResources().getString(R.string.AboutAlertMessage));
+        alertDialog.setButton(getResources().getString(R.string.OK),
+            new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int which)
+                    {
                     };
 
             } );
